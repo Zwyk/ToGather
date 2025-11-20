87899166eb1c86a6f1f88e1d21dafa1db3bdf3d3
Zwyk <erb.fel@gmail.com>
2025-11-20 17:21:02 +0100

    Refactor map configuration and styling for improved responsiveness and theme support

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 window.GOOGLE_MAPS_API_KEY = "AIzaSyDGdXQvBwzzwSYYPBEYH7OnxnyLDee3yu4";
-window.GOOGLE_MAP_ID = "baa119da52cbd0d227176dfd";
+window.GOOGLE_MAP_ID_LIGHT = "baa119da52cbd0d227176dfd";
+window.GOOGLE_MAP_ID_DARK = "3eaec891cedf494388426fc8";
+window.GOOGLE_MAP_ID = window.GOOGLE_MAP_ID_LIGHT;
